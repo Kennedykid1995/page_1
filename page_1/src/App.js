9426253c33da949cf855ceea7098c3be30049b84
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-12-15 20:41:16 -0600

    CSS Grid
    Tried out CSS Grid layout

---
+++ page_1/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div class="grid-layout">
+    <div class="grid-item yellow"></div>
+    <div class="grid-item red"></div>
+    <div class="grid-item span-3 blue"></div>
+    <div class="grid-item yellow"></div>
+    <div class="grid-item span-2 red"></div>
+    <div class="grid-item blue"></div>
+    <div class="grid-item yellow"></div>
+    <div class="grid-item red"></div>
+    <div class="grid-item blue"></div>
+    <div class="grid-item span-2 red"></div>
+    <div class="grid-item yellow"></div>
+    <div class="grid-item blue"></div>
+    <div class="grid-item red"></div>
+    <div class="grid-item yellow"></div>
+    <div class="grid-item blue"></div>
+    <div class="grid-item span-2 yellow"></div>
+    <div class="grid-item red"></div>
+    <div class="grid-item yellow"></div>
+    <div class="grid-item blue"></div>
+    <div class="grid-item span-2 red"></div>
+    <div class="grid-item yellow"></div>
+    <div class="grid-item blue"></div>
+    <div class="grid-item red"></div>
+    <div class="grid-item span-3 blue"></div>
+    <div class="grid-item yellow"></div>
+    <div class="grid-item red"></div>
+</div>
+    );
+  }
+}
+
+export default App;
